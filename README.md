4b0787b4d02450768b1b9502128604b887b518b4
Adam <snescoder@gmail.com>
2015-02-15 17:23:31 -0500

    Untested first draft, MIT license.

---
+++ README.md
@@ -0,0 +1,27 @@
+# sm-energy-sizing
+
+Changes to how much energy E-Tanks and Reserve Tanks grant at pickup in Super
+Metroid.
+
+Uses free space in bank 84, since it has to add code to the tanks’ PLMs.
+
+## Single Tank
+
+Instead of a complete refill, regular energy tank pickups only add one tank
+worth to Samus’s current energy.
+
+If you want energy tanks to grant **zero** energy, you can write `EA EA EA` at
+$84:8972 ($20972 file offset, unheadered.)
+
+Uses free space at FFF0.
+
+## Reserves with Energy
+
+Instead of being completely empty, reserve tanks come with exactly one reserve
+tank worth of energy.
+
+Uses free space at FFE0.
+
+# License
+
+MIT.  If it breaks, you get to keep the pieces.
---
+++ reserve-full.xkas06.asm
@@ -0,0 +1,38 @@
+; By [n00btube](https://github.com/n00btube) aka Adam, February 2015.
+
+lorom
+
+; IMPORTANT ADDRESSES:
+; 7E:09D4 - max reserve tank energy
+; 7E:09D6 - current reserve tank energy
+; 84:8986 - where the energy tank PLM changes Samus's energy
+; 84:E909 - size of a reserve energy tank
+; 84:EFD3-FFFF - free space we're going to park code in
+
+; the normal routine calculates the fresh size of a reserve tank, then carries
+; on without leaving us room to mod anything.  So, we'll overwrite its STA
+; with a JSR and then put the STA as our first instruction.  Both the STA and
+; JSR are 3 bytes so that everything lines up perfectly.
+
+; Address of existing instruction (part of the block starting at 84:8968)
+; I guess if you wanted empty e-tanks, you could write EA EA EA in here to
+; replace the STA with NOPs, and you don't need ASM at all for that.
+org $84898D
+JSR hijack_reserve
+
+
+; This is an address you can move around the free space here as desired,
+; you just need 14 bytes ($0E bytes) of room for it.
+org $84FFE0
+
+; add single tank routine
+hijack_reserve:
+STA $09D4    ; instruction we overwrote
+LDA $09D6    ; load current reserves
+; this add re-uses the value that was loaded earlier in the routine, as Y
+; hasn't been changed since then.
+CLC
+ADC $0000,Y  ; add value from 0 bytes after Y register
+STA $09D6    ; save back to current reserves
+RTS          ; done.
+
